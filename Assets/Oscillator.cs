e1f8aa61900f5b314947df72fa80a631a1bb11fa
kostya05983 <kostya05983@mail.ru>
2020-06-12 16:51:47 +0700

    Add sinus for oscillator's movement.

--- Assets/Oscillator.cs
+++ Assets/Oscillator.cs
@@ -5,10 +5,10 @@ using UnityEngine;
 [DisallowMultipleComponent]
 public class Oscillator : MonoBehaviour
 {
-    [SerializeField] private Vector3 movementVector;
+    [SerializeField] private Vector3 movementVector = new Vector3(10f, 10f, 10f);
+    [SerializeField] private float period = 2f;
 
-    // todo remove from inspector later
-    [Range(0, 1)] [SerializeField] float movementFactor; //0 for not moved 1 
+    float movementFactor; //0 for not moved 1 
 
     private Vector3 startingPos;
 
@@ -21,6 +21,16 @@ public class Oscillator : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // set movement factor
+
+        float cycles = Time.time / period; // grows continualty from 0
+
+        const float tau = Mathf.PI * 2;
+        float rawSinWave = Mathf.Sin(cycles * tau);
+
+        print(rawSinWave);
+
+        movementFactor = rawSinWave / 2f + 0.5f;
         Vector3 offset = movementVector * movementFactor;
         transform.position = startingPos + offset;
     }
