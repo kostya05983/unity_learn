b736a66f566e9663c69bcdd91f9db38390f3f210
kostya05983 <kostya05983@mail.ru>
2020-06-19 00:56:09 +0700

    Add vertical movement

--- 4_Argon_Assault/Assets/Player.cs
+++ 4_Argon_Assault/Assets/Player.cs
@@ -7,6 +7,9 @@ public class Player : MonoBehaviour
 {
     [Tooltip("In ms^-1")] [SerializeField] float xSpeed = 4f;
     [Tooltip("In m")] [SerializeField] float xRange = 17f;
+    [Tooltip("In ms^-1")] [SerializeField] private float ySpeed = 4f;
+    [Tooltip("In m")] [SerializeField] float yMinRange = 17f;
+    [Tooltip("In m")] [SerializeField] float yMaxRange = 17f;
 
     // Start is called before the first frame update
     void Start()
@@ -18,8 +21,14 @@ public class Player : MonoBehaviour
     {
         float xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
         float xOffset = xThrow * xSpeed * Time.deltaTime;
-        float rawNewXPos = Mathf.Clamp(transform.localPosition.x + xOffset, -xRange, xRange);
+        float rawNewXPos = transform.localPosition.x + xOffset;
+        float clampXPos = Mathf.Clamp(rawNewXPos, -xRange, xRange);
 
-        transform.localPosition = new Vector3(rawNewXPos, transform.localPosition.y, transform.localPosition.z);
+        float yThrow = CrossPlatformInputManager.GetAxis("Vertical");
+        float yOffset = yThrow * ySpeed * Time.deltaTime;
+        float rawNewYPos = transform.localPosition.y + yOffset;
+        float clampYPos = Mathf.Clamp(rawNewYPos, yMinRange, yMaxRange);
+        
+        transform.localPosition = new Vector3(clampXPos, clampYPos, transform.localPosition.z);
     }
 }
