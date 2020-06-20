bba80215f04dc4495b5bca72abab498e58c60c7e
kostya05983 <kostya05983@mail.ru>
2020-06-20 23:15:33 +0700

    Clean up

--- 4_Argon_Assault/Assets/Scripts/MusicPlayer.cs
+++ 4_Argon_Assault/Assets/Scripts/MusicPlayer.cs
@@ -1,22 +1,9 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.SceneManagement;
+﻿using UnityEngine;
 
 public class MusicPlayer : MonoBehaviour
 {
     private void Awake()
     {
         DontDestroyOnLoad(gameObject);
-    }
-    
-    // Start is called before the first frame update
-    void Start() {
-        Invoke("LoadFirstScene", 2f);
-    }
-
-    void LoadFirstScene()
-    {
-        SceneManager.LoadScene(1);
     }
 }
---
+++ 4_Argon_Assault/Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    // Start is called before the first frame update
+    void Start() {
+        Invoke("LoadFirstScene", 2f);
+    }
+
+    void LoadFirstScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
