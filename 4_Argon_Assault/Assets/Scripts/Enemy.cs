91c466cfcc7ba68e4f5d963bf7fd0a01453a33f2
kostya05983 <kostya05983@mail.ru>
2020-08-03 02:18:24 +0700

    Add hitPoints.

--- 4_Argon_Assault/Assets/Scripts/Enemy.cs
+++ 4_Argon_Assault/Assets/Scripts/Enemy.cs
@@ -5,6 +5,7 @@ public class Enemy : MonoBehaviour
     [SerializeField] GameObject deathFx;
     [SerializeField] Transform parent;
     [SerializeField] int scorePerHit = 12;
+    [SerializeField] int hits = 10;
 
     ScoreBoard scoreBoard;
 
@@ -24,6 +25,15 @@ public class Enemy : MonoBehaviour
     void OnParticleCollision(GameObject other)
     {
         scoreBoard.ScoreHit(scorePerHit);
+        hits--;
+        if (hits <= 1)
+        {
+            KillEnemy();
+        }
+    }
+
+    private void KillEnemy()
+    {
         GameObject fx = Instantiate(deathFx, transform.position, Quaternion.identity);
         fx.transform.parent = parent;
         Destroy(gameObject);
