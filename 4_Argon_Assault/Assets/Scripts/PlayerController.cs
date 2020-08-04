4a554342b53e138f3e2fc0854a71fc95164aa787
kostya05983 <kostya05983@mail.ru>
2020-08-05 00:56:20 +0700

    Fix bug

--- 4_Argon_Assault/Assets/Scripts/PlayerController.cs
+++ 4_Argon_Assault/Assets/Scripts/PlayerController.cs
@@ -78,27 +78,20 @@ public class PlayerController : MonoBehaviour
     {
         if (CrossPlatformInputManager.GetButton("Fire"))
         {
-            ActivateGuns();
+            SetGunsActive(true);
         }
         else
         {
-            DeactivateGuns();
+            SetGunsActive(false);
         }
     }
 
-    private void ActivateGuns()
+    private void SetGunsActive(bool isActive)
     {
         foreach (GameObject gun in guns)
         {
-            gun.SetActive(true);
-        }
-    }
-
-    private void DeactivateGuns()
-    {
-        foreach (GameObject gun in guns)
-        {
-            gun.SetActive(false);
+            var emission = gun.GetComponent<ParticleSystem>().emission;
+            emission.enabled = isActive;
         }
     }
 }
