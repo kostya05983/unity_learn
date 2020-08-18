1d7db2d6f37f8de7a061883c78c557cd34915567
kostya05983 <kostya05983@mail.ru>
2020-08-18 23:39:31 +0700

    Add coordinates.

--- Tower Defense/Assets/CubeEditor.cs
+++ Tower Defense/Assets/CubeEditor.cs
@@ -1,19 +1,30 @@
 ﻿using UnityEngine;
 
 [ExecuteInEditMode]
-public class EditorSnap : MonoBehaviour
+[SelectionBase]
+public class CubeEditor : MonoBehaviour
 {
     [SerializeField] [Range(1f, 20f)] float gridSize = 10f;
+
+
+    private TextMesh _textMesh;
+
+    void Start()
+    {
+    }
 
     void Update()
     {
         Vector3 snapPos;
+
+
         snapPos.x = Mathf.RoundToInt(transform.position.x / gridSize) * gridSize;
         snapPos.y = Mathf.RoundToInt(transform.position.y / gridSize) * gridSize;
 
         snapPos.z = Mathf.RoundToInt(transform.position.z / gridSize) * gridSize;
-
-
         transform.position = new Vector3(snapPos.x, snapPos.y, snapPos.z);
+
+        _textMesh = GetComponentInChildren<TextMesh>();
+        _textMesh.text = snapPos.x / gridSize + "," + snapPos.z / gridSize;
     }
 }
