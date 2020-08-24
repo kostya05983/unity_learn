7ea5bf87434e51af7d62ca2ebce966242ad9f711
kostya05983 <kostya05983@mail.ru>
2020-08-24 22:56:47 +0700

    Add dictionary of map

---
+++ Tower Defense/Assets/PathFinder.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+public class PathFinder : MonoBehaviour
+{
+    Dictionary<Vector2Int, WayPoint> grid = new Dictionary<Vector2Int, WayPoint>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        LoadBlocks();
+    }
+
+    private void LoadBlocks()
+    {
+        var waypoints = FindObjectsOfType<WayPoint>();
+        foreach (var waypoint in waypoints)
+        {
+            var gridPos = waypoint.GetGridPos();
+            if (grid.ContainsKey(gridPos))
+            {
+                Debug.LogWarning("Skipping overlapping block " + waypoint);
+            }
+            else
+            {
+                grid.Add(gridPos, waypoint);
+            }
+        }
+    }
+}
