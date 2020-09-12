9906f3f2a5824260214fa6d5bd987f3accacf5a1
kostya05983 <kostya05983@mail.ru>
2020-09-12 17:00:13 +0700

    Add more particles.

--- Tower Defense/Assets/EnemyDamage.cs
+++ Tower Defense/Assets/EnemyDamage.cs
@@ -6,6 +6,8 @@ public class EnemyDamage : MonoBehaviour
 {
     [SerializeField] Collider collisionMesh;
     [SerializeField] int hitPoints = 10;
+    [SerializeField] ParticleSystem hitParticlePrefab;
+    [SerializeField] ParticleSystem deathParticlePrefab;
 
     // Start is called before the first frame update
     void Start()
@@ -24,10 +26,13 @@ public class EnemyDamage : MonoBehaviour
     void ProcessHit()
     {
         hitPoints = hitPoints - 1;
+        hitParticlePrefab.Play();
     }
 
     private void KillEnemy()
     {
+        var vfx = Instantiate(deathParticlePrefab, transform.position, Quaternion.identity);
+        vfx.Play();
         Destroy(gameObject);
     }
 }
