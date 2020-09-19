5551395094597e9c559e594db568e89b0e5132ac
kostya05983 <kostya05983@mail.ru>
2020-09-19 13:05:25 +0700

    Add ui text.

--- Tower Defense/Assets/EnemySpawner.cs
+++ Tower Defense/Assets/EnemySpawner.cs
@@ -1,16 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EnemySpawner : MonoBehaviour
 {
     [Range(0.1f, 120f)] [SerializeField] float secondsBetweenSpawns = 2f;
     [SerializeField] EnemyMovement enemyPrefab;
     [SerializeField] Transform enemyParentTransform;
+    [SerializeField] Text scoreText;
+
+    private double score = 0;
 
     // Start is called before the first frame update
     void Start()
     {
+        scoreText.text = score.ToString();
         StartCoroutine(RepeatedSpawnEnemies());
     }
 
@@ -18,9 +23,16 @@ public class EnemySpawner : MonoBehaviour
     {
         while (true)
         {
+            IncreaseScore();
             var enemy = Instantiate(enemyPrefab, transform.position, Quaternion.identity);
             enemy.transform.parent = enemyParentTransform;
             yield return new WaitForSeconds(secondsBetweenSpawns);
         }
+    }
+
+    private void IncreaseScore()
+    {
+        score++;
+        scoreText.text = score.ToString();
     }
 }
